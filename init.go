a9d7c348edbc5eb97c08b80d4c87f724eff7306e
Máté Birkás <gadfly16@gmail.com>
2019-06-17 17:49:02 +0200

    Moved initialization out from main

--- init.go
+++ init.go
@@ -1,10 +1,9 @@
-// Copyright © 2019 Máté Birkás <gadfly16@gmail.com>
+// Copyright © 2019 Máté Birkás <gadfly16@gmail.com>.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
+// http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,12 +11,17 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package main
+package teflon
 
 import (
-	"github.com/gradient-images/teflon/teflon/commands"
+	"log"
+	"os"
 )
 
-func main() {
-	commands.Execute()
+func init() {
+	TeflonConf = os.Getenv("TEFLONCONF")
+	if TeflonConf == "" {
+		log.Println("FATAL: TEFLONCONF environment variable is not set.")
+		os.Exit(1)
+	}
 }
